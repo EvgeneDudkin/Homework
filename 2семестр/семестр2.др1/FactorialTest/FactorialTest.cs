e7b9a7c10a3ed07344bfe4f68957c08857405415
Evgene <EvgeneMartyshenko@mail.ru>
2014-03-23 03:37:53 +0400

    добавил 1  задачу

---
+++ 2семестр/семестр2.др1/FactorialTest/FactorialTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using First;
+
+namespace FactorialTest
+{
+    [TestClass]
+    public class FactorialsTest
+    {
+        [TestMethod]
+        public void FactorialTest()
+        {
+            Assert.IsTrue(Factorials.Factorial(5) == 120);
+        }
+    }
+}
---
+++ 2семестр/семестр2.др1/семестр2.др1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter value ");
+            int value = int.Parse(System.Console.ReadLine());
+            if (value < 0)
+            {
+                System.Console.WriteLine("Error");
+            }
+            else
+            {
+                System.Console.WriteLine("Factorial {0} = {1}", value, Factorials.Factorial(value));
+            }
+        }
+    }
+}
