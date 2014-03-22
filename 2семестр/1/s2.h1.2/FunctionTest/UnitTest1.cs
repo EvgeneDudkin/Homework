ecb963ed8eb2a7a8428f35a3607d262842af03a7
Evgene <EvgeneMartyshenko@mail.ru>
2014-03-23 03:57:58 +0400

    2 задача

---
+++ 2семестр/1/s2.h1.2/FunctionTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Second;
+
+namespace FunctionTest
+{
+    [TestClass]
+    public class FunctionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Function.Fibonacci(3) == 2);
+        }
+    }
+}
