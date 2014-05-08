41235e97ad4b201d07688051bd349a532f2f2601
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2014-05-08 17:45:41 +0400

    добвил факториал

---
+++ 2семестр/1/Factorial/Factorial/Factorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    /// <summary>
+    /// Класс, считающий факториал 
+    /// </summary>
+    public class Factorials
+    {
+        /// <summary>
+        /// Берем число и считаем его факториал 
+        /// </summary>
+        /// <param name="value">Число от которого мы считаем факториал</param>
+        /// <returns>факториал</returns>
+        public static int Factorial(int value)
+        {
+            if (value <= 0)
+            {
+                throw new Exception();
+            }
+            if (value == 1)
+            {
+                return 1;
+            }
+            return value * Factorial(value - 1);
+        }
+    }
+}
---
+++ 2семестр/1/Factorial/FactorialTest/FactorialTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using First;
+
+namespace FactorialTest
+{
+    [TestClass]
+    public class FactorialsTest
+    {
+        [TestMethod]
+        public void FactorialTest()
+        {
+            Assert.IsTrue(Factorials.Factorial(5) == 120);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void BadFactorialTest()
+        {
+            Factorials.Factorial(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void FactorialZeroTest()
+        {
+            Factorials.Factorial(0);
+        }
+
+    }
+}
