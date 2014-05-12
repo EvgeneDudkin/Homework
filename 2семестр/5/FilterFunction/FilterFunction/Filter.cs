51af3e602adf3807fc8fb6529de8108f6a02d65a
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2014-05-12 21:25:49 +0400

    добавил 2 задачу из 5 домашки

---
+++ 2семестр/5/FilterFunction/FilterFunction/Filter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterFunction
+{
+    /// <summary>
+    /// Класс необходимой нам функции
+    /// </summary>
+    public class Filter
+    {
+        /// <summary>
+        /// Возвращает список с элементами, удовлетворяющими функции
+        /// </summary>
+        /// <param name="list">список</param>
+        /// <param name="function">функция</param>
+        /// <returns>получившийся список</returns>
+        public static List<int> FilterFunc(List<int> list,Func<int, bool> function)
+        {
+            List<int> resultList = new List<int>();
+            foreach(var x in list)
+            {
+                if (function(x) == true)
+                {
+                    resultList.Insert(resultList.Count, x);
+                }
+            }
+            return resultList;
+        }
+    }
+}
---
+++ 2семестр/5/FilterFunction/FilterTests/FilterFunctionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FilterFunction;
+using System.Collections.Generic;
+
+namespace FilterFunction
+{
+    [TestClass]
+    public class FilterFunctionTests
+    {
+        [TestMethod]
+        public void FilterFunctionTest()
+        {
+            List<int> list = new List<int> { 1, 2, 3 };
+            List<int> result = Filter.FilterFunc(list, x => (x % 2) == 0);
+            Assert.IsTrue(result[0] == 2 && result.Count == 1);
+        }
+    }
+}
