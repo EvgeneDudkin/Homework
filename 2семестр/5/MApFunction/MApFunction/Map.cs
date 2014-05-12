b15488b4ff03822eadbeed257e50c672d46db728
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2014-05-12 20:07:42 +0400

    добавил 1 задачу из 5 домашки

---
+++ 2семестр/5/MApFunction/MApFunction/Map.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MApFunction
+{
+    /// <summary>
+    /// Класс с нужной нам функцией
+    /// </summary>
+    public class Map
+    {
+        /// <summary>
+        /// Возвращает лист с измененными функцией элементами 
+        /// </summary>
+        /// <param name="list">лист</param>
+        /// <param name="function">функция</param>
+        /// <returns>изменый лист</returns>
+        public static List<int> MapFunc(List<int> list, Func<int, int> function)
+        {
+            List<int> resultList = new List<int>();
+            foreach(var x in list)
+            {
+                resultList.Insert(resultList.Count, function(x));
+            }
+            return resultList;
+        }
+    }
+}
---
+++ 2семестр/5/MApFunction/MapTests/MapFunctionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MApFunction;
+using System.Collections.Generic;
+
+namespace MapFunction
+{
+    [TestClass]
+    public class MApFunctionTests
+    {
+        [TestMethod]
+        public void FirstMapTest()
+        {
+            List<int> list = new List<int>() { 1, 2, 3 };
+            List<int> testList = new List<int>();
+            testList = Map.MapFunc(list, x => x * 2);
+            Assert.IsTrue(testList[0] == 2 && testList[1] == 4 && testList[2] == 6);
+        }
+    }
+}
