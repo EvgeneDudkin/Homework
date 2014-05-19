ef090a34479ada0debc4814cc03e08dc2561d328
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2014-05-20 03:40:01 +0400

    добавил 3 задачу из 5 домашки

---
+++ 2семестр/5/FoldFunc/FoldFunc/FoldFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldFunc
+{
+    /// <summary>
+    /// Класс нужной нам фкункции
+    /// </summary>
+    public class FoldFunction
+    {
+        /// <summary>
+        /// Функция Fold
+        /// </summary>
+        /// <param name="list">список, который мы принимаем</param>
+        /// <param name="firstElement">элемент</param>
+        /// <param name="function">функция</param>
+        /// <returns>значение </returns>
+        public static int Fold(List<int> list, int firstElement, Func<int, int, int> function)
+        {
+            foreach (var x in list)
+            {
+                firstElement = function(firstElement, x);
+            }
+            return firstElement;
+        }
+    }
+}
---
+++ 2семестр/5/FoldFunc/FoldFuncTest/FoldTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using FoldFunc;
+
+namespace FoldFunc
+{
+    [TestClass]
+    public class FoldTests
+    {
+        [TestMethod]
+        public void FoldFirstTest()
+        {
+            int result = FoldFunction.Fold(new List<int>() { 1, 2, 3 }, 1, (acc, elem) => acc * elem);
+            Assert.IsTrue(result == 6);
+        }
+    }
+}
