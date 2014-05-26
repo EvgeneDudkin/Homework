f336169e73806092881b14c2f0f7fdb456c88427
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2014-05-26 21:48:36 +0400

    добавил задачи из 7 домашки

---
+++ 2семестр/7/2/Set/Set/Set.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Set
+{
+    /// <summary>
+    /// Класс множеств
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Set<T> : IEnumerable<T>
+    {
+        private HashSet<T> ourSet;
+        public int Size
+        {
+            get
+            {
+                return ourSet.Count;
+            }
+        }
+
+        /// <summary>
+        /// Метод создания множества
+        /// </summary>
+        public Set()
+        {
+            ourSet = new HashSet<T>();
+        }
+
+        /// <summary>
+        /// Метод добавления элемента
+        /// </summary>
+        /// <param name="value"></param>
+        public void Add(T value)
+        {
+            ourSet.Add(value);
+        }
+
+        /// <summary>
+        /// Метод удаления элемента
+        /// </summary>
+        /// <param name="value"></param>
+        public bool Delete(T value)
+        {
+            return ourSet.Remove(value);
+        }
+
+        /// <summary>
+        /// Метод проверки существования элемента в нашем множестве
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Contain(T value)
+        {
+            return ourSet.Contains(value);
+        }
+
+        /// <summary>
+        /// Объединения двух множеств
+        /// </summary>
+        /// <param name="firstSet"></param>
+        /// <param name="secondSet"></param>
+        /// <returns></returns>
+        public static Set<T> Union(Set<T> firstSet, Set<T> secondSet)
+        {
+            Set<T> result = new Set<T>();
+            foreach (var x in firstSet)
+            {
+                result.Add(x);
+            }
+            foreach (var x in secondSet)
+            {
+                if (!result.Contain(x))
+                {
+                    result.Add(x);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Пересечение двух множеств
+        /// </summary>
+        /// <param name="firstSet"></param>
+        /// <param name="secondSet"></param>
+        /// <returns></returns>
+        public static Set<T> Intersection(Set<T> firstSet, Set<T> secondSet)
+        {
+            Set<T> result = new Set<T>();
+            foreach (var x in firstSet)
+            {
+                if (secondSet.Contain(x))
+                {
+                    result.Add(x);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Метод "обнуления"
+        /// </summary>
+        public void Clear()
+        {
+            ourSet.Clear();
+        }
+
+        /// <summary>
+        /// Энумератор
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ourSet.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
---
+++ 2семестр/7/2/Set/SetTests/SetTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Set;
+
+namespace SetTests
+{
+    [TestClass]
+    public class SetTests
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            Set<int> set = new Set<int>();
+            set.Add(1);
+            Assert.IsTrue(set.Size == 1);
+        }
+
+        [TestMethod]
+        public void ContainTest()
+        {
+            Set<int> set = new Set<int>();
+            set.Add(1);
+            Assert.IsTrue(set.Contain(1));
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Set<int> set = new Set<int>();
+            set.Add(3);
+            set.Delete(3);
+            Assert.IsFalse(set.Contain(3));
+            Assert.IsFalse(set.Delete(3));
+        }
+
+        [TestMethod]
+        public void Clear()
+        {
+            Set<int> set = new Set<int>();
+            set.Add(3);
+            set.Clear();
+            Assert.IsTrue(set.Size == 0);
+        }
+
+        [TestMethod]
+        public void UnionTest()
+        {
+            Set<int> firstSet = new Set<int>();
+            Set<int> secondSet = new Set<int>();
+            secondSet.Add(1);
+            secondSet.Add(2);
+            secondSet.Add(3);
+            firstSet.Add(0);
+            firstSet.Add(2);
+            firstSet.Add(3);
+            var result = Set<int>.Union(firstSet, secondSet);
+            Assert.IsTrue(result.Contain(0) && result.Contain(1) && result.Contain(2) && result.Contain(3) && result.Size == 4);
+        }
+
+        [TestMethod]
+        public void IntersectionTest()
+        {
+            Set<int> firstSet = new Set<int>();
+            var secondSet = new Set<int>();
+            secondSet.Add(1);
+            secondSet.Add(2);
+            secondSet.Add(3);
+            firstSet.Add(0);
+            firstSet.Add(2);
+            firstSet.Add(4);
+            var result = Set<int>.Intersection(firstSet, secondSet);
+            Assert.IsTrue(result.Contain(2) && result.Size == 1);
+        }
+    }
+}
