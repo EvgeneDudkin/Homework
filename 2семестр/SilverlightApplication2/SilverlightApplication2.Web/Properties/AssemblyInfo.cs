d0c081e92ec1d080d8f955086ef61b8f5b5f04d5
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2014-05-28 01:02:49 +0400

    добавил 9 домашку

---
+++ 2семестр/SilverlightApplication2/SilverlightApplication2.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("SilverlightApplication2.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SilverlightApplication2.Web")]
+[assembly: AssemblyCopyright("© , 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("561539ec-af25-4c88-8771-7a2a7f2669ca")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 2семестр/SilverlightApplication2/SilverlightApplication2/MainPage.xaml.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightApplication2
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int temp = 0;
+        int test = 0;
+
+        int[,] matrix = new int[3, 3];
+
+        public int Function(int[,] matrix)
+        {
+            if (matrix[0, 0] + matrix[0, 1] + matrix[0, 2] == 3 || matrix[1, 0] + matrix[1, 1] + matrix[1, 2] == 3 || matrix[2, 0] + matrix[2, 1] + matrix[2, 2] == 3 || matrix[0, 0] + matrix[1, 0] + matrix[2, 0] == 3 || matrix[0, 1] + matrix[1, 1] + matrix[2, 1] == 3 || matrix[0, 2] + matrix[1, 2] + matrix[2, 2] == 3 || matrix[0, 0] + matrix[1, 1] + matrix[2, 2] == 3 || matrix[2, 0] + matrix[1, 1] + matrix[0, 2] == 3)
+            {
+                return 1;
+            }
+            if (matrix[0, 0] + matrix[0, 1] + matrix[0, 2] == -3 || matrix[1, 0] + matrix[1, 1] + matrix[1, 2] == -3 || matrix[2, 0] + matrix[2, 1] + matrix[2, 2] == -3 || matrix[0, 0] + matrix[1, 0] + matrix[2, 0] == -3 || matrix[0, 1] + matrix[1, 1] + matrix[2, 1] == -3 || matrix[0, 2] + matrix[1, 2] + matrix[2, 2] == -3 || matrix[0, 0] + matrix[1, 1] + matrix[2, 2] == -3 || matrix[2, 0] + matrix[1, 1] + matrix[0, 2] == -3)
+            {
+                return 2;
+            }
+            else
+                return 0;
+        }
+        private void button1Click(object sender, RoutedEventArgs e)
+        {
+            test++;
+            if (temp == 0 && button1.Content.ToString() != "O")
+            {
+                button1.Content = "X";
+                temp = 1;
+                matrix[0, 0] = 1;
+            }
+            if (temp == 1 && button1.Content.ToString() != "X")
+            {
+                button1.Content = "O";
+                temp = 0;
+                matrix[0, 0] = -1;
+            }
+            if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+            if (test == 9)
+            {
+                MessageBox.Show("No Winners");
+            }
+        }
+
+        private void button2Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button2.Content.ToString() != "O")
+            {
+                button2.Content = "X";
+                temp = 1;
+                matrix[0, 1] = 1;
+            }
+            if (temp == 1 && button2.Content.ToString() != "X")
+            {
+                button2.Content = "O";
+                temp = 0;
+                matrix[0, 1] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+
+        private void button3Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button3.Content.ToString() != "O")
+            {
+                button3.Content = "X";
+                temp = 1;
+                matrix[0, 2] = 1;
+            }
+            if (temp == 1 && button3.Content.ToString() != "X")
+            {
+                button3.Content = "O";
+                temp = 0;
+                matrix[0, 2] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+
+        private void button4Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button4.Content.ToString() != "O")
+            {
+                button4.Content = "X";
+                temp = 1;
+                matrix[1, 0] = 1;
+            }
+            if (temp == 1 && button4.Content.ToString() != "X")
+            {
+                button4.Content = "O";
+                temp = 0;
+                matrix[1, 0] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+
+        private void button5Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button5.Content.ToString() != "O")
+            {
+                button5.Content = "X";
+                temp = 1;
+                matrix[1, 1] = 1;
+            }
+            if (temp == 1 && button5.Content.ToString() != "X")
+            {
+                button5.Content = "O";
+                temp = 0;
+                matrix[1, 1] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+
+        private void button6Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button6.Content.ToString() != "O")
+            {
+                button6.Content = "X";
+                temp = 1;
+                matrix[1, 2] = 1;
+            }
+            if (temp == 1 && button6.Content.ToString() != "X")
+            {
+                button6.Content = "O";
+                temp = 0;
+                matrix[1, 2] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+
+        private void button7Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button7.Content.ToString() != "O")
+            {
+                button7.Content = "X";
+                temp = 1;
+                matrix[2, 0] = 1;
+            }
+            if (temp == 1 && button7.Content.ToString() != "X")
+            {
+                button7.Content = "O";
+                temp = 0;
+                matrix[2, 0] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+
+        private void button8Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button8.Content.ToString() != "O")
+            {
+                button8.Content = "X";
+                temp = 1;
+                matrix[2, 1] = 1;
+            }
+            if (temp == 1 && button8.Content.ToString() != "X")
+            {
+                button8.Content = "O";
+                temp = 0;
+                matrix[2, 1] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+
+        private void button9Click(object sender, RoutedEventArgs e)
+        {
+            if (temp == 0 && button9.Content.ToString() != "O")
+            {
+                button9.Content = "X";
+                temp = 1;
+                matrix[2, 2] = 1;
+            }
+            if (temp == 1 && button9.Content.ToString() != "X")
+            {
+                button9.Content = "O";
+                temp = 0;
+                matrix[2, 2] = -1;
+            }
+             if (Function(matrix) == 1)
+            {
+                MessageBox.Show("Win The First Player");
+                test = 0;
+            }
+             if (Function(matrix) == 2)
+            {
+                MessageBox.Show("Win The Second Player");
+                test = 0;
+            }
+             test++;
+             if (test == 9)
+             {
+                 MessageBox.Show("No Winners");
+             }
+        }
+    }
+}
