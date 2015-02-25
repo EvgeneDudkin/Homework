8079f714d3c947f9cf9c33389c8d975a3ed13644
EvgeneDudkin <EvgeneMartyshenko@mail.ru>
2015-02-25 22:44:26 +0400

    Add first homework

---
+++ 4семестр/1/FIrstHomework/FIrstHomework/Program.fs
@@ -0,0 +1,17 @@
+﻿let rec factorial x =
+ if (x < 1) then 1 
+ else factorial(x-1)*x
+
+let testFactorial = factorial 5
+
+printfn "%s" "Factorial 5 :"
+printfn "%A" testFactorial
+
+let rec fibonacci x =
+ if (x < 3) then 1 
+ else fibonacci(x-1) + fibonacci(x-2)
+
+let testFibonacci = fibonacci 6
+
+printfn "%s" "Fibonacci 6 :"
+printfn "%A" testFibonacci
